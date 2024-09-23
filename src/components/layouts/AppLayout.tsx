5b9c718def0291bd7bda4f968789529d7726cd20
Lachlan MacPhee <github@lachlanmacphee.com>
2024-09-23 23:27:47 +1000

    improve layout

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
-import { Footer } from "./Footer";
 
 export function Applayout() {
-    return (
-      <>
-        <Header />
-        <Outlet />
-        <div className="container px-4 md:px-8">
-          <Footer />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,13 +1,11 @@
 import { NavLink } from "react-router-dom";
-import { cn } from "@/lib/utils";
 import { appConfig } from "@/config/app";
-import { buttonVariants } from "@/components/ui/button";
-import { GithubIcon } from "lucide-react";
+import { ModeToggle } from "../mode-toggle";
 
 export function Header() {
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/90 backdrop-blur">
-      <div className="container px-4 md:px-8 flex h-14 items-center">
+      <div className="container px-4 md:px-8 flex justify-between h-14 items-center">
         <div className="mr-4 hidden md:flex">
           <NavLink to="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold">{appConfig.name}</span>
@@ -16,32 +14,28 @@ export function Header() {
         <a href="/" className="mr-6 flex items-center space-x-2 md:hidden">
           <span className="font-bold inline-block">{appConfig.name}</span>
         </a>
-        {/* right */}
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            {/* <CommandMenu /> */}
-          </div>
-          <nav className="flex items-center space-x-2">
-            <a
-              href={appConfig.github.url}
-              title={appConfig.github.title}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <GithubIcon className="h-4 w-4" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </a>
-          </nav>
-        </div>
+        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Built by{" "}
+          <a
+            href={appConfig.author.url}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            {appConfig.author.name}
+          </a>
+          . Source code is available on{" "}
+          <a
+            href={appConfig.github.url}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <ModeToggle />
       </div>
     </header>
   );
